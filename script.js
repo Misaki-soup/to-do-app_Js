8338f985325fdf49d3b9b9ef381adc6c4201f4dd
Misaki-soup <misaki.irema@email.com>
2026-04-22 13:57:54 +0200

    Fixed done button, added state of done not done tasks

--- script.js
+++ script.js
@@ -5,29 +5,30 @@ let input = document.getElementById("taskInput");
 let addBtn = document.getElementById("addBtn");
 const list = [];
 
-
 window.onload = function loadLocal(){
-    let tasks = localStorage.getItem('1')
+    let tasks = localStorage.getItem('tasks')
     if (tasks === null) {
         return;
     } else {
         list.push(...JSON.parse(tasks))
         for(let i = 0; i<list.length; i++){
-            let task = list.at(i)
-            createTask(task)
+            let task = list.at(i);
+            createTask(task);
     }
     }
 }
 
 addBtn.addEventListener("click", function() {
     let text = input.value;
+    let task = {}
     
-
     if (text === "") return; // ignore empty input
-    list.push(text);
+    task['text'] = text;
+    task['done'] = false;
+    list.push(task);
+    createTask(task);
     saveToLocal(list);
-    createTask(text);
-
+    
     
 
     input.value = "";
@@ -35,22 +36,35 @@ addBtn.addEventListener("click", function() {
 
 
 function saveToLocal(data){
-        localStorage.setItem('1',JSON.stringify(data))
-        console.log (localStorage)
+        localStorage.setItem('tasks',JSON.stringify(data))
 }
 
-function createTask(text){
+function createTask(task){
     let li = document.createElement("li");
-    li.textContent = text+' ';
+    li.textContent = task['text']+' ';
 
     let deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.addEventListener("click", function() {
         li.remove();
+        let index_removed = list.indexOf(task)
+        list.splice(index_removed,1)
+        saveToLocal(list)
+    })
 
+    let doneBtn = document.createElement("button");
+    doneBtn.textContent = "done";
+    doneBtn.addEventListener("click", function(){task['done'] = true;
+        li.style.textDecorationLine='line-through'; 
+        li.style.textDecorationColor = 'red';
+        saveToLocal(list)
     });
+
+    if(task['done']){
+        li.style.textDecorationLine='line-through'; 
+        li.style.textDecorationColor = 'red';}
+    
     li.appendChild(deleteBtn);
+    li.appendChild(doneBtn);
     taskList.appendChild(li);
-
-
 }
